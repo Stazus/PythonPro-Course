377e87c9ae28ede05665d5a90e4ae2b6ff9867e2
Stanisław Flak <stazus@interia.pl>
2026-04-04 17:07:11 +0200

    Add task4_area.py

---
+++ homework/lesson1/task4_area.py
@@ -0,0 +1,5 @@
+side_A_length = float(input('Type side "A" length: ')) # proszę o podanie długości boku "A", który jest liczbą zmiennoprzecinkową
+side_B_length = float(input('Type side "B" length: ')) # prosze o podanie długości boku "B", który jest liczbą zmiennoprzecinkową
+area = side_A_length * side_B_length # pole (obszar) prostokąta bok "A" * bok "B"
+
+print(f'area equals: {area}')
