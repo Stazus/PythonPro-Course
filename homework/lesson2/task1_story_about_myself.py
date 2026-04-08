ae0b91063c1ca1a104ca75617938236d54d5dd09
Stanisław Flak <stazus@interia.pl>
2026-04-09 00:36:28 +0200

    Add lesson2 task1_story_about_myself.py

---
+++ homework/lesson2/task1_story_about_myself.py
@@ -0,0 +1,7 @@
+print("Hi, my name is Stanisław.")
+
+print("My hobby is programming.")
+
+print("I'm learning Python because I want to become a Python Developer.")
+
+print("I believe that systematic learning will lead me to my goal.")
