70c59c72043f50d3f8d37f95c03321c7e5e761b0
Stanisław Flak <stazus@interia.pl>
2026-04-09 01:53:27 +0200

    Add lesson2 task4_character_codes.py

---
+++ homework/lesson2/task4_character_codes.py
@@ -0,0 +1,7 @@
+name = input("Type your name: ")
+
+first_letter = name[0]
+
+# Display ASCII codes for uppercase and lowercase letters
+print(f"ASCII code of an uppercase letter '{first_letter.upper()}' is: {ord(first_letter.upper())}")
+print(f"ASCII code of a lowercase letter '{first_letter.lower()}' is. {ord(first_letter.lower())}")
