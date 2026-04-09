6354cdf4df7776e4babb9b247791c965a1b92eb6
Stanisław Flak <stazus@interia.pl>
2026-04-10 01:44:02 +0200

    Add lesson2 task10_temperature_converter.py

---
+++ homework/lesson2/task10_temperature_converter.py
@@ -0,0 +1,8 @@
+# Taking the temperature in degrees Celsius
+celsius = float(input("Type the temperature in degrees Celsius: "))
+
+# Conversion to Fahrenheit
+fahrenheit = celsius * 9/5 + 32
+
+# Display result with explanation
+print(f"{celsius}°C is {fahrenheit}°F according to the formula: F = C * 9/5 + 32")
