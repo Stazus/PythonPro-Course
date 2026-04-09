16106d22a5778c1f49b9faaf48aaadcf825de783
Stanisław Flak <stazus@interia.pl>
2026-04-09 02:14:08 +0200

    Add lesson2 task5_character_creator.py

---
+++ homework/lesson2/task5_character_creator.py
@@ -0,0 +1,4 @@
+character_race = input("Type your character's race (e.g. Elf, Dwarf): ")
+character_class = input("Type your character's class (e.g. Warrior, Mage): ")
+
+print(f"A character has been created: {character_race} {character_class}")
