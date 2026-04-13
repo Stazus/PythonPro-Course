816bf7fb47070d0edfacbadf5fc2121d774d00ce
Stanisław Flak <stazus@interia.pl>
2026-04-13 22:57:57 +0200

    Add lesson3 task4_praca_z_f-stringami.py

---
+++ homework/lesson3/task4_praca_z_f-stringami.py
@@ -0,0 +1,7 @@
+imie = input("Podaj swoje imię: ")
+
+rok_urodzenia = int(input("Podaj swój rok urodzenioa: "))
+
+wiek = 2025 - rok_urodzenia
+
+print(f"Cześć, {imie}! W 2025 roku bedziesz mieć około {wiek} lat.")
