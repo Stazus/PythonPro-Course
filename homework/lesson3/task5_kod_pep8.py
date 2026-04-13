aa134dc2761058140ad3dcd677d811e699a205c4
Stanisław Flak <stazus@interia.pl>
2026-04-13 23:14:42 +0200

    Add lesson3 task5_kod_pep8.py

---
+++ homework/lesson3/task5_kod_pep8.py
@@ -0,0 +1,11 @@
+"""
+Kod źródłowy
+NazwaUzytkownika="JanKowalski"
+wiekUzytkownika=25
+if wiekUzytkownika>=18:print(NazwaUzytkownika+' jest dorosły.')
+"""
+Nazwa_Uzytkownika = "Jan Kowaliski"
+wiek_Uzytkownika = 25
+
+if wiek_Uzytkownika >= 18:
+    print(Nazwa_Uzytkownika + ' jest dorosły.')
