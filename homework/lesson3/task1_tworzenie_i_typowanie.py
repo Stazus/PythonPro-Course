d90a37103e6934a655bd0f7a45212ebf356d8309
Stanisław Flak <stazus@interia.pl>
2026-04-13 22:08:30 +0200

    Add lesson3 task1_tworzenie_i_typowanie.py

---
+++ homework/lesson3/task1_tworzenie_i_typowanie.py
@@ -0,0 +1,10 @@
+imie = "Stanisław"
+wiek = 53
+srednia_ocen = 4.5
+czy_student = True
+
+print(f'Imię: {imie} Typ: {type(imie)}')
+print(f'Wiek: {wiek} Typ: {type(wiek)}')
+print(f'Średnia ocen: {srednia_ocen} Typ: {type(srednia_ocen)}')
+print(f'Status studenta: {czy_student} Typ: {type(czy_student)}')
+      
