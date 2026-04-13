5acf1531cf4deae2ed0774f16eca346f622b0956
Stanisław Flak <stazus@interia.pl>
2026-04-13 23:55:01 +0200

    Add task8_rzutowanie_typow.py

---
+++ homework/lesson3/task8_rzutowanie_typow.py
@@ -0,0 +1,10 @@
+liczba_str = "5.8"
+print("liczba_str: ", liczba_str)
+
+liczba_float = float(liczba_str)
+print("Po konwersji na float: ", liczba_float)
+
+liczba_int = int(liczba_float)
+print("Po konwersji na int: ", liczba_int)
+
+# Konwersja z float na int powoduje usunięcie części dziesiętnej
