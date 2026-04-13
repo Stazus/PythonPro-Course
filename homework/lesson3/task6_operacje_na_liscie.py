040712ebdfc44c4165b8d8931627e994e1e93e8b
Stanisław Flak <stazus@interia.pl>
2026-04-13 23:27:54 +0200

    Add task6_operacje_na_liscie.py

---
+++ homework/lesson3/task6_operacje_na_liscie.py
@@ -0,0 +1,10 @@
+owoce = ["jabłko", "banan", "wisnia"]
+print("Po utworzeniu listy: ", owoce)
+
+owoce.append("pomatańcza")
+print("Po dodaniu 'pomarańcza':", owoce)
+
+owoce[1] = "jagoda"
+print("Po zamianie 'banan' na 'jagoda':", owoce)
+
+print("Końcowa lista owoców:", owoce)
