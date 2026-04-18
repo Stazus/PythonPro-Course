ab1cde2917a4a96c58788912544ba58858be8bc2
Stanisław Flak <stazus@interia.pl>
2026-04-18 14:34:29 +0200

    Add task6_prawda_czy_falsz.py

---
+++ homework/lesson4/task6_prawda_czy_falsz.py
@@ -0,0 +1,7 @@
+tekst = input("Wpisz dowolny tekst: ")
+
+# Sprawdzenie czy tekst jest "prawdziwy" (czy nie jest pusty)
+if bool(tekst):
+    print("Wpisałeś tekst, więc to jest prawda (True).")
+else:
+    print("Nic nie wpisałeś, więc to jest fałsz (False).")
