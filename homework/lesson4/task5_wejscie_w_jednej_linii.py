4b1fe5e4ee10708fabb4a9bd0791fd1da2418611
Stanisław Flak <stazus@interia.pl>
2026-04-18 14:23:45 +0200

    Add task5_wejscie_w_jednej_linii.py

---
+++ homework/lesson4/task5_wejscie_w_jednej_linii.py
@@ -0,0 +1,7 @@
+# Wyświetlenie pytania bez przejścia do nowej linii
+print("Podaj swoje imię: ", end="")
+
+# Pobranie odpowiedzi od uzytkownika (wpisuje w tej samej linii)
+imie = input()
+
+print("Cześć,", imie +"!")
