720bf08ccd3b4bf6be03b241c5a248648e018acc
Stanisław Flak <stazus@interia.pl>
2026-04-24 22:18:47 +0200

    Add task9_potegi_dwojki.py

---
+++ homework/lesson5/task9_potegi_dwojki.py
@@ -0,0 +1,9 @@
+liczba = 1 # zaczynamy od 2^0 = 1
+
+while liczba < 1000:
+    print(liczba)
+    liczba *= 2 
+    """ 
+    w kazdej iteracji jest drukowana aktualna wartość, a następnie jest mnożona przez 2 
+    (przejście do kolejnej potegi dwójki)
+    """
