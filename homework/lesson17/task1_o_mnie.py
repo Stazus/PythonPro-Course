d30cd845737c2a48638a79ed812026c06fe2f85b
Stanisław Flak <stazus@interia.pl>
2026-05-30 18:58:03 +0200

    Add lesson17 task2_prosty_kalkulator

--- homework/lesson17/task1_o_mnie.py
+++ homework/lesson17/task1_o_mnie.py
@@ -10,5 +10,11 @@ def home():
 def me():
     return "Stanisław Flak"
 
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    suma = num1 + num2
+    return f"Wynik to: {suma}"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
