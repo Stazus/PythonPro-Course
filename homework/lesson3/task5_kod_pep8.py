966317a25c9b7ce78907d5ad3bd3a3b295f66c84
Stanisław Flak <stazus@interia.pl>
2026-06-08 17:51:55 +0200

    Poprawienie task5_kod_pep8.py z lesson3

--- homework/lesson3/task5_kod_pep8.py
+++ homework/lesson3/task5_kod_pep8.py
@@ -4,8 +4,8 @@ NazwaUzytkownika="JanKowalski"
 wiekUzytkownika=25
 if wiekUzytkownika>=18:print(NazwaUzytkownika+' jest dorosły.')
 """
-Nazwa_Uzytkownika = "Jan Kowaliski"
-wiek_Uzytkownika = 25
+nazwa_uzytkownika = "Jan Kowaliski"
+wiek_uzytkownika = 25
 
-if wiek_Uzytkownika >= 18:
-    print(Nazwa_Uzytkownika + ' jest dorosły.')
+if wiek_uzytkownika >= 18:
+    print(nazwa_uzytkownika + ' jest dorosły.')
