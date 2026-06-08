b9380b4f6bd1a68c4ff469379876202febee18eb
Stanisław Flak <stazus@interia.pl>
2026-06-08 18:54:07 +0200

    Poprawienie task7_niemutowalnosc_krotki.py z lesson3

--- homework/lesson3/task7_niemutowalnosc_krotki.py
+++ homework/lesson3/task7_niemutowalnosc_krotki.py
@@ -1,3 +1,10 @@
 punkt = (10, 20, 30)
 
-punkt[0] = 15 # TypeError: 'tuple' object does not support item assignment - błąd ponieważ krotka jest niemutowalnaS
+# Krotki (tuple) są niemutowalne, co oznacza, że po ich utworzeniu
+# nie można zmieniać, dodawać ani usuwać ich elementów.
+# Poniższa instrukcja spowodowałaby błąd:
+# TypeError: 'tuple' object does not support item assignment
+
+# punkt[0] = 15
+
+print(punkt)
