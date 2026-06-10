7206acb8673d7e3068e11cd3d0b5814a7d780fe1
Stanisław Flak <stazus@interia.pl>
2026-06-11 00:39:46 +0200

    Dodanie paginacji listy notatek

--- homework/lesson20/notatnik/views.py
+++ homework/lesson20/notatnik/views.py
@@ -1,10 +1,21 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Note
 
 
 def note_list(request):
     notes = Note.objects.all()
-    return render(request, "note_list.html", {"notes": notes})
+
+    paginator = Paginator(notes, 3)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "note_list.html",
+        {"page_obj": page_obj}
+    )
 
 
 def note_detail(request, note_id):
