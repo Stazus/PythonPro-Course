37343177f7f37d260ad4edf486bfab1031afa213
Stanisław Flak <stazus@interia.pl>
2026-06-20 21:32:48 +0200

    Dodano task7_relacja_i_wyswietlanie

--- homework/lesson21/notatnik/models.py
+++ homework/lesson21/notatnik/models.py
@@ -13,3 +13,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
+    
