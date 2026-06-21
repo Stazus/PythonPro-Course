732bc673ed0551c775911120a37e6bbdf0418f80
Stanisław Flak <stazus@interia.pl>
2026-06-21 19:49:17 +0200

    Dodano lesson22_task8_tagi_postow

--- homework/lesson22/notatnik/admin.py
+++ homework/lesson22/notatnik/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Note, Category, Article
+from .models import Note, Category, Article, Post, Tag
 
 admin.site.register(Note)
 admin.site.register(Category)
 admin.site.register(Article)
+admin.site.register(Post)
+admin.site.register(Tag)
