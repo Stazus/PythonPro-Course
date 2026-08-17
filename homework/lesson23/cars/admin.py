145a9d1565567da794bd1bbcf47249a98f9ee0a1
Stanisław Flak <stazus@interia.pl>
2026-08-18 01:42:24 +0200

    Lekcja 23 - zadanie 8

--- homework/lesson23/cars/admin.py
+++ homework/lesson23/cars/admin.py
@@ -6,7 +6,7 @@ from .models import Car
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("brand", "model", "year", "availability_status", "car_age")
+    list_display = ("brand", "model", "year", "availability_status", "car_age", "photo_thumbnail")
     search_fields = ("brand", "model")
     list_filter = ("year", "is_available")
     ordering = ("-year", "brand")
@@ -20,3 +20,12 @@ class CarAdmin(admin.ModelAdmin):
         if obj.is_available:
             return format_html('<span style="color: green;">Dostępny</span>')
         return format_html('<span style="color: red;">Niedostępny</span>')
+
+    @admin.display(description="Zdjęcie")
+    def photo_thumbnail(self, obj):
+        if obj.photo:
+            return format_html(
+                '<img src="{}" width="100" height="60" style="object-fit: cover;">',
+                obj.photo.url,
+            )
+        return "Brak zdjęcia"
