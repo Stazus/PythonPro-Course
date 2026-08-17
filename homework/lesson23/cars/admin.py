6183c2924494b60dffdf704d612e6d55ff5f6c07
Stanisław Flak <stazus@interia.pl>
2026-08-18 01:17:58 +0200

    Lekcja 23 - zadanie 6

--- homework/lesson23/cars/admin.py
+++ homework/lesson23/cars/admin.py
@@ -1,10 +1,15 @@
+from datetime import date
+
 from django.contrib import admin
 from .models import Car
 
-
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("brand", "model", "year", "is_available")
+    list_display = ("brand", "model", "year", "is_available", "car_age")
     search_fields = ("brand", "model")
     list_filter = ("year", "is_available")
     ordering = ("-year", "brand")
+
+    @admin.display(description="Wiek samochodu")
+    def car_age(self, obj):
+        return date.today().year - obj.year
