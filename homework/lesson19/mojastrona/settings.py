ee2c0ec36c179701aea9fbb3544bcd4a771c7fad
Stanisław Flak <stazus@interia.pl>
2026-08-18 00:02:36 +0200

    Poprawiono zadania 4 i 10 z lekcji 19

--- homework/lesson19/mojastrona/settings.py
+++ homework/lesson19/mojastrona/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'mojastrona.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moja_baza',
+        'USER': 'moj_uzytkownik',
+        'PASSWORD': 'moje_haslo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- homework/lesson19/ogloszenia/admin.py
+++ homework/lesson19/ogloszenia/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Ogloszenie
 
-admin.site.register(Ogloszenie)
+
+@admin.register(Ogloszenie)
+class OgloszenieAdmin(admin.ModelAdmin):
+    list_display = ("tytul", "cena", "data_dodania")
