29f417ebb8ac05eddd764811942bbe1dc279227b
Stanisław Flak <stazus@interia.pl>
2026-08-21 21:53:24 +0200

    Lekcja 24 - zadanie 3

--- homework/lesson24/mojastrona/urls.py
+++ homework/lesson24/mojastrona/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import profile
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('profile/', profile, name='profile'),
 ]
