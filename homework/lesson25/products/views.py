8b687b77ef1b5e3e9cd253a6222f39790e68174b
Stanisław Flak <stazus@interia.pl>
2026-08-22 16:45:56 +0200

    Lekcja 25 - zadanie 8

--- homework/lesson25/products/views.py
+++ homework/lesson25/products/views.py
@@ -10,6 +10,19 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        min_price = self.request.query_params.get("min_price")
+        max_price = self.request.query_params.get("max_price")
+
+        if min_price:
+            queryset = queryset.filter(price__gte=min_price)
+
+        if max_price:
+            queryset = queryset.filter(price__lte=max_price)
+
+        return queryset
+
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
