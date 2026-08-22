ab884977be64c4532c120d62b882ae2a7ef4861f
Stanisław Flak <stazus@interia.pl>
2026-08-22 14:23:03 +0200

    Lekcja 25 - zadanie 3

--- homework/lesson25/mojastrona/urls.py
+++ homework/lesson25/mojastrona/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from products.views import ProductViewSet
+
+
+router = DefaultRouter()
+router.register("products", ProductViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
