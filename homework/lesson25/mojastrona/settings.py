ea5e9db59f1b0bb4f05863739276d14d5c848337
Stanisław Flak <stazus@interia.pl>
2026-08-22 13:42:49 +0200

    Lekcja 25 - zadanie 2

--- homework/lesson25/mojastrona/settings.py
+++ homework/lesson25/mojastrona/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'products',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
