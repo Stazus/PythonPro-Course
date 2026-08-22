6c70c7cb4f0d72fc3e8dfea081549d42f534853b
Stanisław Flak <stazus@interia.pl>
2026-08-22 15:29:04 +0200

    Lekcja 25 - zadanie 5

--- homework/lesson25/mojastrona/urls.py
+++ homework/lesson25/mojastrona/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from products.views import ProductViewSet
-
+from products.views import ProductViewSet, hello, set_name
 
 router = DefaultRouter()
 router.register("products", ProductViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/hello/", hello, name="hello"),
+    path("api/set-name/", set_name, name="set_name"),
     path("api/", include(router.urls)),
 ]
--- homework/lesson25/products/views.py
+++ homework/lesson25/products/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -7,3 +9,17 @@ from .serializers import ProductSerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+@api_view(["GET"])
+def set_name(request):
+    name = request.query_params.get("name", "Gość")
+    response = Response({"message": f"Ustawiono imię: {name}"})
+    response.set_cookie("user_name", name)
+    return response
+
+
+@api_view(["GET"])
+def hello(request):
+    name = request.COOKIES.get("user_name", "Gość")
+    return Response({"message": f"Witaj, {name}!"})
