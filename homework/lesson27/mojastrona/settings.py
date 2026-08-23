82036d4c2e87fad3bd1e62caef193b83e75e0545
Stanisław Flak <stazus@interia.pl>
2026-08-23 12:22:44 +0200

    Lekcja 27 - zadanie 10

--- homework/lesson27/mojastrona/settings.py
+++ homework/lesson27/mojastrona/settings.py
@@ -151,8 +151,11 @@ SIMPLE_JWT = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
-        "LOCATION": BASE_DIR / "file_cache",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
     }
 }
 
