f4282cc30e68909d656261e2f6c134c8ee186544
Stanisław Flak <stazus@interia.pl>
2026-08-23 14:03:19 +0200

    Lekcja 29 - zadanie 1

---
+++ homework/lesson29/api/views.py
@@ -0,0 +1,102 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+import time
+
+from django.contrib.auth import get_user_model
+from django.core.cache import cache
+
+from rest_framework import viewsets
+
+from .models import Product
+from .serializers import ProductSerializer
+
+from django.http import JsonResponse
+from .tasks import hello_world
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @method_decorator(cache_page(60))
+    def get(self, request):
+        print("WYKONANO WIDOK - CACHE MISS")
+        return Response({
+            "username": request.user.username
+        })
+
+
+
+class SelectiveCacheView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        User = get_user_model()
+
+        # Proste i szybkie zapytanie do bazy - wykonuje się za każdym razem
+        users_count = User.objects.count()
+        print("WYKONANO ZAPYTANIE DO BAZY")
+
+        # Cachujemy tylko wynik kosztownego obliczenia
+        cache_key = "complex_calculation_result"
+        complex_result = cache.get(cache_key)
+
+        if complex_result is None:
+            print("CACHE MISS - wykonuję skomplikowane obliczenia")
+            time.sleep(3)
+            complex_result = 42
+            cache.set(cache_key, complex_result, 60)
+        else:
+            print("CACHE HIT - wynik skomplikowanych obliczeń z cache")
+
+        return Response({
+            "users_count": users_count,
+            "complex_result": complex_result,
+        })
+
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    @method_decorator(cache_page(60 * 10))
+    def list(self, request, *args, **kwargs):
+        print("PRODUCT LIST - wykonano widok")
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        product_id = kwargs["pk"]
+        cache_key = f"product_detail_{product_id}"
+
+        cached_data = cache.get(cache_key)
+
+        if cached_data is not None:
+            print(f"PRODUCT {product_id} - CACHE HIT")
+            return Response(cached_data)
+
+        print(f"PRODUCT {product_id} - CACHE MISS")
+
+        product = self.get_object()
+        serializer = self.get_serializer(product)
+
+        cache.set(cache_key, serializer.data, 60)
+
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        product = serializer.save()
+
+        cache_key = f"product_detail_{product.pk}"
+        cache.delete(cache_key)
+
+        print(f"USUNIĘTO CACHE DLA PRODUKTU {product.pk}")
+
+def hello_world_view(request):
+    hello_world.delay()
+
+    return JsonResponse({
+        "message": "Zadanie hello_world zostało wysłane do Celery."
+    })
---
+++ homework/lesson29/mojastrona/urls.py
@@ -0,0 +1,48 @@
+"""
+URL configuration for mojastrona project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.views import (
+    ProtectedView,
+    SelectiveCacheView,
+    ProductViewSet,
+    hello_world_view,
+)
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('api/protected/', ProtectedView.as_view(), name='protected'),
+    path("__debug__/", include("debug_toolbar.urls")),
+    path(
+        'api/selective-cache/',
+        SelectiveCacheView.as_view(),
+        name='selective-cache'
+),
+    path('api/', include(router.urls)),
+    path(
+        "api/hello-celery/",
+        hello_world_view,
+        name="hello-celery",
+),
+]
