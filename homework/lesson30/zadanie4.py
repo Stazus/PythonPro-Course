bbbc6ccfa48148fd2b2314d9d7fad6b6628fc82b
Stanisław Flak <stazus@interia.pl>
2026-08-29 00:47:57 +0200

    Lekcja 30 - zadanie 4

---
+++ homework/lesson30/zadanie4.py
@@ -0,0 +1,27 @@
+import threading
+
+
+lista = []
+
+
+def dodaj_jedynki():
+    for _ in range(100_000):
+        lista.append(1)
+
+
+def dodaj_dwojki():
+    for _ in range(100_000):
+        lista.append(2)
+
+
+watek1 = threading.Thread(target=dodaj_jedynki)
+watek2 = threading.Thread(target=dodaj_dwojki)
+
+watek1.start()
+watek2.start()
+
+watek1.join()
+watek2.join()
+
+print(f"Długość listy: {len(lista)}")
+print(f"Czy lista ma 200000 elementów? {len(lista) == 200_000}")
