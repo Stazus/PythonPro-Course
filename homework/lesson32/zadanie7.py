b24107ea9d285bd5ec74d089357fff13c3f0e33b
Stanisław Flak <stazus@interia.pl>
2026-08-29 14:15:00 +0200

    Lekcja 32 - zadanie 7

---
+++ homework/lesson32/zadanie7.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100))
+    price: Mapped[int] = mapped_column(Integer)
+
+
+print(Product.__tablename__)
+print(Product.__table__)
