1057d81501fc8b0af451b71df32a4fd651999121
Stanisław Flak <stazus@interia.pl>
2026-08-30 10:29:34 +0200

    Lekcja 33 - zadanie 2

---
+++ homework/lesson33/zadanie2.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Path
+
+
+app = FastAPI()
+
+
+@app.get("/greet/{name}")
+async def greet(
+    name: Annotated[str, Path(min_length=2)]
+):
+    return {"message": f"Hello, {name}!"}
