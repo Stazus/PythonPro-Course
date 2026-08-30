bb470779cdb42a6750b2c137caeb8a12afc6d409
Stanisław Flak <stazus@interia.pl>
2026-08-30 13:54:33 +0200

    Lekcja 34 - zadanie 5

---
+++ homework/lesson34/zadanie5/app.py
@@ -0,0 +1,43 @@
+import strawberry
+from aiohttp import web
+from strawberry.aiohttp.views import GraphQLView
+
+
+@strawberry.type
+class User:
+    id: strawberry.ID
+    name: str
+    email: str
+
+
+users = [
+    User(id="1", name="Jan Kowalski", email="jan@example.com"),
+    User(id="2", name="Anna Nowak", email="anna@example.com"),
+]
+
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def user(self, id: strawberry.ID) -> User | None:
+        for user in users:
+            if user.id == id:
+                return user
+        return None
+
+    @strawberry.field
+    def users(self) -> list[User]:
+        return users
+
+schema = strawberry.Schema(query=Query)
+
+app = web.Application()
+app.router.add_route(
+    "*",
+    "/graphql",
+    GraphQLView(schema=schema),
+)
+
+
+if __name__ == "__main__":
+    web.run_app(app, host="localhost", port=8080)
